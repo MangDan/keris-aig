0ca6769e8769396b113ebcf78e22778bbea1555b
mangdan <dan.donghu.kim@gmail.com>
2019-12-20 22:44:09 +0900

    2019-12-20 22:44:09

--- keris-aig-frontend/src/main.js
+++ keris-aig-frontend/src/main.js
@@ -13,7 +13,7 @@ axios.defaults.headers.get["Content-Type"] = "application/json";
 // Axios에서 DefaultURL 외에 여러개 URL등록할 때 이런식으로 사용....
 // 호출할때는 this.$detection...
 const detection = axios.create({
-  baseURL: 'http://221.163.201.60:11975',
+  baseURL: 'http://132.145.90.23:11975',
   //baseURL: 'http://221.163.201.60:11715',
   headers: {
     'Content-Type': 'multipart/form-data'
@@ -21,7 +21,7 @@ const detection = axios.create({
 });
 
 const generation = axios.create({
-  baseURL: 'http://221.163.201.60:11976',
+  baseURL: 'http://132.145.90.23:11976',
   //baseURL: 'http://221.163.201.60:11716',
   headers: {
     'Content-Type': 'multipart/form-data'
@@ -30,7 +30,7 @@ const generation = axios.create({
 
 const iscream = axios.create({
   //baseURL: 'http://cms.aimath.home-learn.com',
-  baseURL: 'http://61.74.183.29:11720',
+  baseURL: 'http://132.145.90.23:11720',
   headers: {
     'Content-Type': 'multipart/form-data'
   }
