e0ac0f1623c87d37035653f2156b318056641387
mangdan <dan.donghu.kim@gmail.com>
2019-12-20 11:25:31 +0900

    2019-12-20 11:25:31

--- keris-aig-frontend/src/components/Header.vue
+++ keris-aig-frontend/src/components/Header.vue
@@ -14,6 +14,6 @@
     </div>
     <v-spacer></v-spacer>
 
-    <span class="mr-2 headline font-weight-thin">매스지니</span>
+    <span class="mr-2 headline font-weight-thin">MathGenie</span>
   </v-app-bar>
 </template>
